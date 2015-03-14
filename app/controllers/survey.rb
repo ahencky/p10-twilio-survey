27b0dd509b2c72e727a5ef5334b3df1cba6f4b6e
Afton Hencky <ahencky@gmail.com>
2015-03-13 20:47:28 -0700

    removing chart min and max scale fixes load error

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -28,17 +28,15 @@ end
 post '/chart' do
     id = params[:id]
     @question = Question.find(id)
-    @answers_array = ParticipantAnswer.where(question_id: id).to_a
-    if @answers_array
+    if @question.participant_answers
+        @answers_array = ParticipantAnswer.where(question_id: id).to_a
         @answers = @answers_array.map{|user_answer| user_answer.answer.downcase.lstrip.rstrip unless user_answer.answer.nil?}
         @frequencies = {}
         @answers.map{ |answer| @frequencies[answer] = @answers.count(answer)}
         @answers.delete(nil)
-        @min = @answers.min
-        @max = @answers.max
         if request.xhr?
            content_type :json
-          {question: @question.body, answers: @answers, frequencies: @frequencies, min: @min, max: @max}.to_json
+          {question: @question.body, answers: @answers, frequencies: @frequencies}.to_json
         else
           redirect "/"
         end
